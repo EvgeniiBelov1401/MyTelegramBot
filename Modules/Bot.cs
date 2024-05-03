e7f80925dff80a4c9d849ca5e4323629624bac5f
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-03 14:47:03 +0300

    создал класс Bot

---
+++ Modules/Bot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace MyTelegramBot.Modules
+{
+    internal class Bot
+    {
+        private ITelegramBotClient _telegramClient;
+        public Bot(ITelegramBotClient telegramClient)
+        {
+            _telegramClient = telegramClient;
+        }
+    }
+}
